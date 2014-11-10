817c1292d74a2e398259f9df5a33de8b4c5b8848
Pete <yhteydenotot@luontotupa.net>
2014-11-10 22:47:48 +0200

    Finnish language file
    100% translated and ready

---
+++ language/finnish/lang.favorites.php
@@ -0,0 +1,254 @@
+<?php
+
+ /**
+ * Solspace - Favorites
+ *
+ * @package		Solspace:Favorites
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://www.solspace.com/docs/addon/c/Favorites/
+ * @version		3.0.5
+ * @filesource 	./system/expressionengine/third_party/favorites/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"favorites_module_name" =>
+"Suosikit",
+
+"favorites_module_description" =>
+"Mahdollistaa sivustosi jäsenten tallentaa omia suosikkijulkaisuja heidän Suosikit-listalleen.",
+
+'favorites_module_version' =>
+"Suosikit",
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'homepage' =>
+"Etusivu",
+
+'online_documentation' =>
+"Käyttöohjeet verkossa",
+
+'add_favorite'				=>
+"Lisää artikkeli automaattisesti julkaisijan suosikkilistalle?",
+
+"favorites"							=>
+"Suosikit",
+
+"home"								=>
+"Koti",
+
+"statistics"						=>
+"Yhteenveto",
+
+"module_version"					=>
+"Moduulin versio",
+
+"total_favorites"					=>
+"Suosikkeja yhteensä",
+
+"total_entries_saved"				=>
+"Tallennettuja julkaisuja yhteensä",
+
+"percent_entries_saved"				=>
+"prosenttiosuus tallennetuista julkaisuista",
+
+"top_5"								=>
+"Top 5",
+
+"no_entries_saved"					=>
+"Ei yhtään tallennettua julkaisua",
+
+"members"							=>
+"Jäsenet",
+
+"entries"							=>
+"Julkaisut",
+
+"preferences"						=>
+"Asetukset",
+
+"general_preferences"				=>
+"Yleiset Asetukset",
+
+
+//----------------------------------------
+//	Preferences
+//----------------------------------------
+
+"pref_name"							=>
+"Nimi",
+
+"pref_value"						=>
+"Arvo",
+
+"show_favorites"					=>
+"Näytä Suosikit",
+
+"documentation"						=>
+"Käyttöohjeet verkossa",
+
+"prefs"								=>
+"Asetukset",
+
+"language"							=>
+"Kieli",
+
+"no_string"							=>
+"Virhe: Ei dataan liittyvää query stringiä",
+
+"no_login"							=>
+"Virhe: Käyttäjä ei ole kirjautunut",
+
+"no_id"								=>
+"Virhe: Ei olemassaolevaa julkaisun id-numeroa",
+
+"id_not_found"						=>
+"Virhe: Julkaisua ei löytynyt",
+
+"no_duplicates"						=>
+"Virhe: Tämä on jo lisätty käyttäjän Suosikkeihin",
+
+"no_favorites"						=>
+"Yhtään Suosikkia ei ole lisätty.",
+
+"no_delete"							=>
+"Virhe: Suosikkia ei löydy eikä sitä voida poistaa.",
+
+"success_add"						=>
+"Onnistui: Suosikki on lisätty onnistuneesti",
+
+"success_delete"					=>
+"Onnistui: Suosikki on poistettu onnistuneesti",
+
+"success_update"					=>
+"Asetukset on päivitetty!",
+
+"thank_you"							=>
+"Kiitos",
+
+"success_delete_all"					=>
+"Onnistui: Kaikki tallentamasi suosikit on poistettu onnistuneesti.",
+
+//----------------------------------------
+//	Members
+//----------------------------------------
+
+"member"							=>
+"Jäsen",
+
+"count"								=>
+"Määrä",
+
+"name"								=>
+"Nimi",
+
+"join_date"							=>
+"Liittymispäivä",
+
+"total_favorites_saved"				=>
+"Suosikkeja tallennettu yhteensä",
+
+"prefs_updated"				=>
+"Asetukset päivitetty",
+
+
+"no_member_id"						=>
+"Jäsenen ID-numeroa ei löydetty. Ole hyvä ja kokeile uudelleen.",
+
+"no_member"							=>
+"Jäsentä ei löytynyt.",
+
+//----------------------------------------
+//	Entries
+//----------------------------------------
+
+"entries"							=>
+"Julkaisut",
+
+"entry"								=>
+"Julkaisu",
+
+"entry_id"							=>
+"Julkaisun Id",
+
+"entry_date"						=>
+"Julkaisupäivä",
+
+"title"								=>
+"Otsikko",
+
+"author"							=>
+"Kirjoittaja",
+
+"total_favorites"					=>
+"Suosikkeja yhteensä",
+
+"no_entry"							=>
+"Julkaisua ei löytynyt.",
+
+"no_entry_id"						=>
+"Julkaisun id-numeroa ei löytynyt. Ole hyvä ja yritä uudelleen.",
+
+//----------------------------------------
+//	Main
+//----------------------------------------
+
+"no_prefs"							=>
+"Asetuksia ei ole vielä määritelty.",
+
+'unsupported_mysql_version' =>
+"Käytössäsi olevaa MySQL ei tueta. &nbsp; Ole hyvä ja ota yhteyttä palveluntarjoajaasi jotta voit päivittää MySQL version vastaamaan tämäm moduulin vaatimuksia.",
+
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+"Tallenna",
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'invalid_request' =>
+"Viallinen Pyyntö",
+
+'favorites_module_disabled' =>
+"Suosikit Moduuli on tällä hetkellä pois käytöstä. Ole hyvä ja varmista ylläpitopaneelin kautta että se on asennettu sekä että moduulin versio on uusin saatavilla oleva.",
+
+'disable_module_to_disable_extension' =>
+"Jotta voit poistaa tämän lisäosan käytöstä, sinun tulee ensin sitä vastaava <a href='%url%'>Moduuli</a>.",
+
+'enable_module_to_enable_extension' =>
+"Jotta saat tämän lisäosan asennettua ja otettua käyttöön, sinun tulee asentaa myös <a href='%url%'>Moduuli</a>.",
+
+'cp_jquery_requred' =>
+"Lisäosa 'jQuery for the Control Panel' tulee olla <a href='%extensions_url%'>asennettu ja käytössä</a> jotta voit käyttää tätä moduulia.",
+
+
+/** --------------------------------------------
+/**  Update Routine
+/** --------------------------------------------*/
+
+'update_favorites' =>
+"Päivitä Suosikit moduuli",
+
+'update_favorites_module' =>
+"Päivitä Suosikit moduuli",
+
+'favorites_update_message' =>
+"Olet ladannut lähiaikoina uudemman version Suosikit moduulista, ole hyvä ja klikkaa tästä jotta päivitys voidaan suorittaa.",
+
+
+/* END */
+''=>''
+);
+?>
